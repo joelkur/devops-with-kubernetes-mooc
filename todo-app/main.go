8c93e8c7eb7b86e3b37a87d9f182b30f42081a89
jkur <me@jkur.dev>
2024-12-07 22:17:04 +0200

    exercise 1.05: add GET handler to todo-app

--- todo-app/main.go
+++ todo-app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -12,6 +13,17 @@ func main() {
 	if !isPortDefined {
 		port = "3000"
 	}
+
+	tmpl := template.Must(template.ParseFiles("index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			status := http.StatusMethodNotAllowed
+			http.Error(w, http.StatusText(status), status)
+		} else {
+			tmpl.Execute(w, nil)
+		}
+	})
 
 	log.Printf("Server started in port %s", port)
 	err := http.ListenAndServe(":"+port, nil)
