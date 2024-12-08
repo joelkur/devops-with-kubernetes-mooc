2d244476a22fc4d352bb37de93f453cb065ae841
jkur <me@jkur.dev>
2024-12-08 10:06:58 +0200

    exercise 1.07: add server to log-output app that responds with current status

--- log-output/main.go
+++ log-output/main.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
-	"github.com/google/uuid"
+	"log"
+	"net/http"
+	"os"
 	"time"
+
+	"github.com/google/uuid"
 )
 
+func getOutputWithTimestamp(s string) string {
+	timestamp := time.Now().Format(time.RFC3339)
+	return fmt.Sprintf("%s: %s", timestamp, s)
+}
+
 func main() {
-	randomString := uuid.New()
-	for {
-		fmt.Printf("%s: %s\n", time.Now().Format(time.RFC3339), randomString.String())
-		time.Sleep(5 * time.Second)
+	randomString := uuid.New().String()
+	output := getOutputWithTimestamp(randomString)
+
+	go func() {
+		for {
+			output = getOutputWithTimestamp(randomString)
+			fmt.Println(output)
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	port, portFromEnv := os.LookupEnv("PORT")
+	if !portFromEnv {
+		port = "3001"
 	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s", output)
+	})
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
